a45ddfc9c638a9b52d50e644772b2cf1284f7d35
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-26 18:16:24 +0200

    Kártya osztály létrehozása

---
+++ src/hu/petrik/bankiszolgaltatasok/Kártya.java
@@ -0,0 +1,27 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class Kártya extends BankiSzolgáltatás {
+
+    private Számla s;
+    private String kartyaSzam;
+
+    public Kártya(Tulajdonos t, Számla S, String kartyaSzam) {
+        super(t);
+        this.s = s;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean Vásárlás(double osszeg) {
+        Számla s = new Számla(super.getT());
+        if (s.Kivesz(osszeg)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
