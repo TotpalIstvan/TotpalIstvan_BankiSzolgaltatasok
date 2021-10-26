801780beda13a984ab10df20e25f52b718965af6
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-26 18:07:05 +0200

    MegtakarításiSzámla létrehozása

---
+++ src/hu/petrik/bankiszolgaltatasok/MegtakarításiSzámla.java
@@ -0,0 +1,28 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class MegtakarításiSzámla extends Számla {
+    private double kamatMertek;
+    private static double kamatAlap;
+
+    public MegtakarításiSzámla(Tulajdonos t, double kamatAlap) {
+        super(t);
+        this.kamatMertek = kamatAlap;
+    }
+
+    public double getKamatMertek() {
+        return kamatMertek;
+    }
+
+    public void setKamatMertek(double kamatMertek) {
+        this.kamatMertek = kamatMertek;
+    }
+
+    public double jovairas() {
+        if (Kivesz(this.kamatMertek)) {
+            setKamatMertek(this.kamatMertek*this.kamatAlap);
+            return getKamatMertek();
+        } else {
+            return getKamatMertek();
+        }
+    }
+}
