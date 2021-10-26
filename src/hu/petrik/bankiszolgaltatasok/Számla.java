3c9027f1256ca940f258d384af7653117281c87f
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-26 17:49:36 +0200

    Számla példányosítás

---
+++ src/hu/petrik/bankiszolgaltatasok/Számla.java
@@ -0,0 +1,20 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class Számla extends BankiSzolgáltatás{
+    private double egyenleg;
+
+    public Számla(Tulajdonos t) {
+        super(t);
+    }
+
+    public double getEgyenleg() {
+        return egyenleg;
+    }
+
+    public double Befizet(double osszeg) {
+        egyenleg += osszeg;
+        return egyenleg;
+    }
+
+
+}
